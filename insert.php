05eded98dd355e03b7dc3f8fd8509cff715e8fc9
rafascards <rafascards@gmail.com>
2020-06-10 19:11:33 -0300

    meu primeiro commit

--- insert.php
+++ insert.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- {{-- jquery and bootstrap --}} -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -12,39 +11,13 @@
     <script src="https://kit.fontawesome.com/886bf3cc12.js" crossorigin="anonymous"></script>
 </head>
 <body class="container">
-
-<?php
-    //mostrar a lista de tarefas
-    $tarefas = [
-        0 => [
-            'id' => 1,
-            'nome' => 'lavar a louça',
-            'descricao' => 'mãe pediu para lavar a louça antes de ela chegar',
-            'completa' => false
-        ],
-        1 => [
-            'id' => 2,
-            'nome' => 'lavar o carro',
-            'descricao' => 'lave o carro',
-            'completa' => false
-        ]
-    ];
-?>
-
-<h1>Lista de Tarefas</h1>
-<h4>Sua lista de tarefas aqui!</h4>
-<hr>
-<a class="btn btn-primary" href="./insert.php">Cadastrar</a>
-<br>
-<?php
-
-foreach ($tarefas as $key => $tarefa) {
-    echo $tarefa['nome'].' - '.$tarefa['descricao'].'<br>';
-}
-
-?>
-
-
-
+    
+    <form action="" method="">
+        <label for="nome">Nome</label>
+        <input type="text" name="nome" id="">
+        <label for="descricao">Descrição</label>
+        <input type="text" name="descricao" id="">
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 </html>
